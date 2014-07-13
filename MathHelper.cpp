0c6877661458cb82f3fd2e792b4e6fff6735ff37
ofirzaibert <ofirzaibert@gmail.com>
2014-07-13 11:22:36 +0300

    Update MathHelper.cpp
    Added implementation for GetIndexFromRadius.

--- MathHelper.cpp
+++ MathHelper.cpp
@@ -71,3 +71,50 @@ double MathHelper::ConvertRadianToDegree(double radian)
 	return radian * 180.0 / M_PI;
 
 }
+
+pair<double,double> MathHelper::GetIndexFromRadius(double dDistanceRadius, double dRobotWidth)
+{
+	// Getting half of width
+	double dHalfWidth = dRobotWidth / 2;
+
+	// Getting X and Y points that intersect the circle radius
+	// http://mathworld.wolfram.com/Circle-LineIntersection.html
+
+	// Getting 2 points of the vertical line
+	Point pntLineFirstPoint(dHalfWidth,0);
+
+	Point pntLineSecondPoint(dHalfWidth, dDistanceRadius * 2);
+
+	// Getting intersection with radius
+	double dX = pntLineSecondPoint.getX() - pntLineFirstPoint.getX();
+	double dY = pntLineSecondPoint.getY() - pntLineFirstPoint.getY();
+
+	double dR = sqrt(pow(dX,2) + pow(dY,2));
+
+	double D = (pntLineFirstPoint.getX() *  pntLineSecondPoint.getY()) - (pntLineSecondPoint.getX() * pntLineFirstPoint.getY());
+
+	double dXIntersect = (D * dY + dX * sqrt(pow(dDistanceRadius,2) * pow(dR,2) - pow(D,2))) / pow(dR,2);
+	double dYIntersect = (-D * dX + dY * sqrt(pow(dDistanceRadius,2) * pow(dR,2) - pow(D,2))) / pow(dR,2);
+
+	// Calculating angle
+	// http://stackoverflow.com/a/1211243
+
+	// Positive X
+	Point P1(0,0);
+	Point P2(dXIntersect, dYIntersect);
+	Point P3(pntLineFirstPoint.getX(), pntLineFirstPoint.getY());
+
+	double P12 = sqrt(pow(P2.getX()-P1.getX(),2) + pow(P2.getY()-P1.getY(),2));
+	double P23 = sqrt(pow(P3.getX()-P2.getX(),2) + pow(P3.getY()-P2.getY(),2));
+	double P13 = sqrt(pow(P1.getX()-P3.getX(),2) + pow(P1.getY()-P3.getY(),2));
+
+	double dPositiveAngle = acos((pow(P12,2)+pow(P13,2)-pow(P23,2)) / (2 *P12 * P13));
+
+	// Negative X
+	double dNegativeAngle = -dPositiveAngle;
+
+	pair<double,double> Angles;
+	Angles = make_pair(dPositiveAngle, dNegativeAngle);
+
+	return Angles;
+}
